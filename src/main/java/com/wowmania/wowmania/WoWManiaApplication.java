ca5ed50bc93d5eaba2c8a72260c8bfbb2531bea0
mateir-7 <radu.matei.05.21@gmail.com>
2025-05-18 23:55:18 +0300

    First Commit

---
+++ src/main/java/com/wowmania/wowmania/WoWManiaApplication.java
@@ -0,0 +1,13 @@
+package com.wowmania.wowmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoWManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoWManiaApplication.class, args);
+	}
+
+}
